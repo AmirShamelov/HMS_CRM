608d41cb19e8af46785c2c681cccfd7d98b3e0ba
amir <am.shamelov@gmail.com>
2025-01-16 05:57:12 +0500

    initialize Django and Vue

---
+++ hms_frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Navbar/>
+
+        <section class="section">
+            <RouterView/>
+        </section>
+    </div>
+
+</template>
+
+<script>
+import Navbar from "@/components/layout/Navbar.vue"
+import axios from "axios"
+
+export default {
+    name: 'App',
+    components: {
+        Navbar
+    },
+    beforeCreate() {
+        this.$store.commit('initializeStore')
+
+        if (this.$store.state.token) {
+            axios.defaults.headers.common['Authorization'] = 'Token' + this.$store.state.token
+        } else {
+            axios.defaults.headers.common['Authorization'] = ''
+        }
+    },
+}
+</script>
+
+
+<style lang="css">
+@import "../node_modules/bulma";
+</style>
+
+
+
+
+
---
+++ hms_frontend/src/main.js
@@ -0,0 +1,13 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import store from "./store";
+import router from './router'
+import axios from "axios"
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+const app = createApp(App).use(store)
+
+app.use(router, axios)
+
+app.mount('#app')
